bffce0865db412ac66352f5df17db39c03a662e0
Istvan Novak <dotneteer@hotmail.com>
2022-07-01 09:43:12 +0200

    fix: copy the bass.dll file (Windows) to the output to allow sound rendering with BASS.

--- source/SpectrumEngine.Client.Avalonia/Program.cs
+++ source/SpectrumEngine.Client.Avalonia/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.IO;
+using System.Reflection;
 using Avalonia;
 using Avalonia.ReactiveUI;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 
 namespace SpectrumEngine.Client.Avalonia
 {
@@ -10,11 +13,29 @@ namespace SpectrumEngine.Client.Avalonia
         // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
         // yet and stuff might break.
         [STAThread]
-        public static void Main(string[] args) => BuildAvaloniaApp()
-            .StartWithClassicDesktopLifetime(args);
+        public static void Main(string[] args)
+        {
+            // --- Prepare BASS resources to provide audio on Windows, Mac, and Linux
+            var asm = Assembly.GetEntryAssembly();
+
+            var targetFolder = Path.GetDirectoryName(asm!.Location);
+            
+            var bassDllName = $"{asm.GetName().Name}.BassResources.bass.dll";
+            var resStream = asm.GetManifestResourceStream(bassDllName);
+            var targetName = Path.Combine(targetFolder!, "bass.dll");
+            if (!File.Exists(targetName) && resStream != null)
+            {
+                resStream.Seek(0, SeekOrigin.Begin);
+                using var fs = new FileStream(targetName, FileMode.OpenOrCreate);
+                resStream.CopyTo(fs);
+            }
+            
+            // --- Now, start the application
+            BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
+        }
 
         // Avalonia configuration, don't remove; also used by visual designer.
-        public static AppBuilder BuildAvaloniaApp()
+        private static AppBuilder BuildAvaloniaApp()
             => AppBuilder.Configure<App>()
                 .UsePlatformDetect()
                 .LogToTrace()
