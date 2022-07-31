c86d7fd7c90de6a25b223bc10aeccb909dbc67d6
Istvan Novak <dotneteer@hotmail.com>
2022-07-31 08:27:16 +0200

    feat: implement the Help command (#40)

--- source/SpectrumEngine.Client.Avalonia/Controls/DevTools/CommandConsole/Commands/HelpCommand.cs
+++ source/SpectrumEngine.Client.Avalonia/Controls/DevTools/CommandConsole/Commands/HelpCommand.cs
@@ -1,5 +1,8 @@
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using SpectrumEngine.Tools.Commands;
+using SpectrumEngine.Tools.Output;
 
 namespace SpectrumEngine.Client.Avalonia.Controls.DevTools;
 
@@ -9,9 +12,47 @@ namespace SpectrumEngine.Client.Avalonia.Controls.DevTools;
 [CommandUsage("help [command ID]")]
 public class HelpCommand: InteractiveCommandBase
 {
+    private string? _commandIdArg;
+    
+    protected override Task<List<ValidationMessage>> ValidateArgs(List<Token> args)
+    {
+        if (args.Count > 1)
+        {
+            return Task.FromResult(new List<ValidationMessage>
+            {
+                new(ValidationMessageType.Error, "This command accepts zero or one argument")
+            });
+        }
+
+        _commandIdArg = args.Count == 0 ? null : args[0].Text;
+        return SuccessMessage;
+    }
+
+
     protected override Task<InteractiveCommandResult> DoExecute(IInteractiveCommandContext context)
     {
-        context.Output?.WriteLine("Help Command executing...");
-        return Task.FromResult(new InteractiveCommandResult(true));
+        var output = context.Output;
+        if (output == null) return SuccessResult;
+        
+        var commandCount = 0;
+        var commands = CommandRegistry.Commands;
+        foreach (var key in commands.Keys.OrderBy(k => k))
+        {
+            if (_commandIdArg != null && !key.ToLower().StartsWith(_commandIdArg.ToLower())) continue;
+            var command = commands[key];
+            output.ResetFormat();
+            output.Color = OutputColors.BrightMagenta;
+            output.Italic = true;
+            output.Write(command.Id);
+            output.Color = OutputColors.Magenta;
+            output.Italic = false;
+            output.Write($" [{string.Join(", ", command.Aliases)}]: ");
+            output.Color = OutputColors.Green;
+            output.WriteLine(command.Description);
+            WriteInfo(context, $"Usage: {command.Usage}");
+            commandCount++;
+        }
+        WriteInfo(context, $"Displayed {commandCount} command{(commandCount > 1 ? "s" : "")}");
+        return SuccessResult;
     }
 }
