5a7db8405f06d95280d7a533e35fbff75b7575b7
David Gonzalez Zornoza <dgzornoza@dgzornoza.com>
2022-08-24 20:01:04 +0200

    implemented cpc disk base code

---
+++ source/SpectrumEngine.Emu/Extensions/BytesExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpectrumEngine.Emu.Extensions
+{
+    public static class BytesExtensions
+    {
+		/// <summary>
+		/// Returns an uint16 from a byte array based on offset
+		/// </summary>
+		public static ushort GetWordValue(this byte[] buf, int offsetIndex)
+		{
+			return (ushort)(buf[offsetIndex] | buf[offsetIndex + 1] << 8);
+		}
+	}
+}
---
+++ source/SpectrumEngine.Emu/Machines/Disk/IPortIODevice.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpectrumEngine.Emu.Machines.Disk
+{
+	/// <summary>
+	/// Represents a device that utilizes port IN &amp; OUT
+	/// </summary>
+	public interface IPortIODevice
+	{
+		/// <summary>
+		/// Device responds to an IN instruction
+		/// </summary>
+		bool ReadPort(ushort port, ref int result);
+
+		/// <summary>
+		/// Device responds to an OUT instruction
+		/// </summary>
+		bool WritePort(ushort port, int result);
+	}
+}
